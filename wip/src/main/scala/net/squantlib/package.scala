4632b6eb6a904495466a7abbe52a521039da0259
Claudio Baraldi <cebaraldi@gmail.com>
2016-04-03 10:59:11 +0200

    Added module examples and package object package.scala

---
+++ wip/src/main/scala/net/squantlib/package.scala
@@ -0,0 +1,19 @@
+package org
+
+/**
+  * Created by ceb on 02/04/16.
+  */
+package object qls {
+  type Integer = Int
+  type BigInteger = Long
+  type Natural = Double // >= 0
+  type Real = Double
+  type Decimal = Double
+  type size_t = Int // >= 0
+  type Time = Double
+  type DiscountFactor = Double
+  type Rate = Double
+  type Spread = Double
+  type Volatility = Double
+  type Probability = Double
+}
